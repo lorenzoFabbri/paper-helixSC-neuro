27f9b56cf373f8679d0c4d0501349ca163cfeeb8
Lorenzo Fabbri <lorenzo.fabbri@isglobal.org>
2023-05-31 14:19:09 +0200

    Modified renv by installing myphd package with remotes.

--- run.R
+++ run.R
@@ -1,10 +1,11 @@
-Sys.setenv(is_hpc = TRUE)
+Sys.setenv(is_hpc = FALSE)
 
 path_store <- ifelse(
   Sys.getenv(is_hpc), 
   "~/PROJECTES/HELIX_OMICS/DATA_PREVIOUS_MIGRATION/lorenzoF_phd/data/data_paper3/_targets/_targetsRQ1", 
   "~/mounts/PROJECTES/HELIX_OMICS/DATA_PREVIOUS_MIGRATION/lorenzoF_phd/data/data_paper3/_targets/_targetsRQ1"
 )
+Sys.setenv(TAR_PROJECT = "rq1")
 targets::tar_make(script = "_targets_rq1.R", 
                   store = path_store)
 targets::tar_destroy(destroy = "all")
