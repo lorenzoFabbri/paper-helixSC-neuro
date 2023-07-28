16f142a20bceda6d9636d13f627a970068bc9038
Lorenzo Fabbri <lorenzo.fabbri@isglobal.org>
2023-07-28 18:09:47 +0200

    RQ3 does not work.

--- run.R
+++ run.R
@@ -17,7 +17,7 @@ path_store_res <- ifelse(
 Sys.setenv(path_store_res = path_store_res)
 ################################################################################
 
-rq <- "2"
+rq <- "3"
 Sys.setenv(TAR_PROJECT = paste0("rq0", rq))
 store <- paste0(path_store, paste("0", rq))
 targets::tar_make(script = "_targets_rq0X.R", 
