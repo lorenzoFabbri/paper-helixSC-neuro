1bcf820c7ab86965b871af818cbb3a68127df1cd
lorenzoFabbri <lorenzo.fabbri@isglobal.org>
2023-11-08 15:27:55 +0100

    Checked compliance to guidenlines for figures and tables.

--- code/proc_res.R
+++ code/proc_res.R
@@ -242,6 +242,116 @@ tbl_desc_vars <- function() {
 
 #' Title
 #'
+#' @param path_store 
+#' @param rq 
+#' @param sa_var 
+#'
+#' @return
+#' @export
+load_res_weighted_fits <- function(path_store, rq, sa_var) {
+  # Load results weighting
+  targets::tar_load(
+    dplyr::contains(paste0("rq", substr(rq, 1, 1), "_weighted_fits_")),
+    store = paste0(path_store, rq)
+  )
+  res_list <- mget(ls(
+    pattern = paste0("rq", substr(rq, 1, 1), "_weighted_fits_*")
+  ))
+  names_ <- gsub(paste0("rq", substr(rq, 1, 1), "_weighted_fits_"),
+                 "",
+                 names(res_list))
+  
+  # Extract weights
+  weights_ <- lapply(1:1, function(idx) {
+    outcome <- gsub(paste0("rq", substr(rq, 1, 1), "_weighted_fits_"),
+                    "",
+                    names_[idx])
+    tmp <- lapply(res_list[[idx]]$fits, "[[", "weights")
+    tmp <- lapply(seq_along(tmp), function(idx2) {
+      ret <- tibble::tibble(tmp[[idx2]])
+      colnames(ret) <- names(tmp)[idx2]
+      return(ret)
+    }) |>
+      dplyr::bind_cols()
+    # Check whether is SA and eventually add effect modifier
+    if (!is.null(sa_var)) {
+      tmp <- tmp |>
+        tidylog::mutate(
+          modifier = res_list[[1]]$fits[[1]]$dat[[sa_var]]
+        ) |>
+        dplyr::relocate(modifier)
+    }
+    tmp <- tmp |>
+      tidylog::mutate(
+        outcome = names_[[idx]]
+      ) |>
+      dplyr::relocate(outcome)
+  }) |> # End loop extract balancing weights
+    dplyr::bind_rows()
+  
+  # Tidy values of effect modifier
+  if (!is.null(sa_var)) {
+    if (sa_var == "e3_sex") {
+      weights_ <- weights_ |>
+        tidylog::mutate(
+          modifier = dplyr::case_when(
+            modifier == 0 ~ "males",
+            modifier == 1 ~ "females",
+            .default = modifier
+          )
+        )
+    } # End if for e3_sex
+  } # End check if SA
+  
+  # Tidy data
+  weights_wide <- weights_
+  weights_ <- weights_ |>
+    tidylog::mutate(
+      outcome = gsub("_", " ", outcome)
+    ) |>
+    tidyr::pivot_longer(
+      cols = -dplyr::any_of(c("outcome", "modifier"))
+    ) |>
+    tidylog::mutate(
+      variable = name,
+      outcome = replace(
+        outcome, outcome == "x11bhsd", "11bhsd"
+      ),
+      variable = replace(
+        variable, variable == "x11bhsd", "11bhsd"
+      ),
+      variable = gsub("hs_", "", variable),
+      variable = gsub("_c", "", variable),
+      variable = gsub("_", " ", variable),
+      outcome = gsub("_", " ", outcome)
+    ) |>
+    tidylog::select(-name)
+  ## Eventually add information on EDCs (chemical classes)
+  if (rq %in% c("1", "2")) {
+    info_edcs <- myphd::edcs_information() |>
+      tibble::as_tibble()
+    weights_ <- weights_ |>
+      tidylog::left_join(
+        info_edcs |>
+          tidylog::select(chem_id, class),
+        by = c("variable" = "chem_id")
+      )
+  } else {
+    weights_ <- weights_ |>
+      tidylog::mutate(
+        class = c("TMP")
+      )
+  }
+  
+  return(list(
+    dat_tbl = weights_wide,
+    dat_plt = weights_
+  ))
+} # End function load_res_weighted_fits
+################################################################################
+
+#' Title
+#'
 #' @param dat_tbl
 #' @param dat_plt
 #' @param sa_var
@@ -345,6 +455,129 @@ tidy_res_weighted_fits <- function(dat_tbl, dat_plt, sa_var = NULL) {
     plot = plot
   ))
 } # End function tidy_res_weighted_fits
+################################################################################
+
+#' Title
+#'
+#' @param path_store 
+#' @param rq 
+#' @param sa_var 
+#' @param which_res 
+#'
+#' @return
+#' @export
+load_res_meffects <- function(path_store, rq, sa_var, which_res) {
+  # Load results
+  targets::tar_load(
+    dplyr::contains(paste0("rq", substr(rq, 1, 1), "_marginal_")),
+    store = paste0(path_store, rq)
+  )
+  marginal_effects <- mget(ls(
+    pattern = paste0("rq", substr(rq, 1, 1), "_marginal_*")
+  ))
+  names_ <- gsub(paste0("rq", substr(rq, 1, 1), "_marginal_"),
+                 "",
+                 names(marginal_effects))
+  
+  # Loop over outcomes
+  ret <- lapply(seq_along(marginal_effects), function(idx) {
+    outcome <- gsub(paste0("rq", substr(rq, 1, 1), "_marginal_"),
+                    "",
+                    names(marginal_effects[idx]))
+    if (outcome != "x11bhsd") {
+      outcome <- gsub("hs", "", outcome)
+    } else {
+      outcome <- "11bhsd"
+    }
+    # Table for one outcome and all exposures
+    x <- marginal_effects[[idx]]
+    if (length(x$marginal_effects) == 0) return(NULL)
+    df <- lapply(x$marginal_effects, "[[", which_res) |>
+      dplyr::bind_rows() |>
+      tidylog::mutate(
+        outcome = outcome,
+        variable = gsub("hs_", "", variable),
+        variable = gsub("_c", "", variable)
+      ) |>
+      tidylog::select(-dplyr::any_of(
+        c("contrast", "statistic")
+      ))
+    if (which_res == "hypothesis") {
+      df$variable <- names(x$marginal_effects)
+    }
+    return(df)
+  }) # End loop over results of marginal effects
+  
+  # Tidy
+  names(ret) <- names_
+  all_res <- purrr::reduce(ret, dplyr::bind_rows) |>
+    tidylog::mutate(
+      variable = gsub("hs_", "", variable),
+      variable = gsub("_c", "", variable),
+      variable = gsub("_", " ", variable),
+      outcome = gsub("_", " ", outcome)
+    )
+  if (!is.null(sa_var) & which_res != "hypothesis") {
+    all_res <- all_res |>
+      tidylog::rename_with(
+        ~ c("modifier"), dplyr::all_of(c(sa_var))
+      )
+  }
+  ## Eventually add information on EDCs (chemical classes)
+  if (rq %in% c("1", "2")) {
+    info_edcs <- myphd::edcs_information() |>
+      tibble::as_tibble()
+    df <- all_res |>
+      tidylog::left_join(
+        info_edcs |>
+          tidylog::select(chem_id, class),
+        by = c("variable" = "chem_id")
+      ) |>
+      tidylog::mutate(
+        outcome = replace(
+          outcome, outcome == "x11bhsd", "11bhsd"
+        )
+      )
+  } else {
+    df <- all_res |>
+      tidylog::mutate(
+        class = c("TMP"),
+        variable = replace(
+          variable, variable == "x11bhsd", "11bhsd"
+        )
+      )
+  }
+  df <- df |>
+    tidylog::mutate(
+      dplyr::across(
+        c("class", "variable"),
+        \(x) {
+          x = factor(
+            x, levels = sort(unique(x))
+          )
+        }
+      ),
+      dplyr::across(
+        dplyr::where(is.character),
+        \(x) stringr::str_trim(x, side = "both")
+      )
+    )
+  ## Tidy values of effect modifier
+  if (!is.null(sa_var) & which_res != "hypothesis") {
+    if (sa_var == "e3_sex") {
+      df <- df |>
+        tidylog::mutate(
+          modifier = dplyr::case_when(
+            modifier == 0 ~ "males",
+            modifier == 1 ~ "females",
+            .default = modifier
+          )
+        )
+    } # End if for e3_sex
+  }
+  
+  return(df)
+} # End function load_res_meffects
 ################################################################################
 
 #' Title
