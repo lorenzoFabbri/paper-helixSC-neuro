7cc0575302b88315cfdda89dd63fa8a77387c73d
lorenzoFabbri <lorenzo.fabbri@isglobal.org>
2023-12-14 11:04:51 +0100

    Rendered manuscript.

--- output/paper/render.R
+++ output/paper/render.R
@@ -1,4 +1,2 @@
-#quarto::quarto_render(input = here::here("output/paper/figures.qmd"))
-#quarto::quarto_render(input = here::here("output/paper/tables.qmd"))
-quarto::quarto_render(input = here::here("output/paper/si.qmd"))
+quarto::quarto_render(input = here::here("output/paper/_si.qmd"))
 quarto::quarto_render(input = here::here("output/paper/manuscript.qmd"))
